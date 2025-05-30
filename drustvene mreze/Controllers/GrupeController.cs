b8cf9223b018d17dd07c9d87b7454ad760cd5e8f
Kilarus97 <zecmirko97@gmail.com>
2025-05-30 14:31:44 +0200

    Feat : Dodata funkcionalnost da grupe rade sa bazon podataka.

--- drustvene mreze/Controllers/GrupeController.cs
+++ drustvene mreze/Controllers/GrupeController.cs
@@ -2,6 +2,7 @@
 using drustvene_mreze.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 
 namespace drustvene_mreze.Controllers
 {
@@ -12,8 +13,7 @@ namespace drustvene_mreze.Controllers
         [HttpGet]
         public ActionResult<List<Grupe>> GetAll()
         {
-            GrupeRepozitorijum grupeRepozitorijum = new GrupeRepozitorijum();
-            List<Grupe> grupe = GrupeRepozitorijum.Podaci.Values.ToList();
+            List<Grupe> grupe = GetAllFromDatabase();
 
             return Ok(grupe);
         }
@@ -46,6 +46,48 @@ namespace drustvene_mreze.Controllers
             return NoContent();
         }
 
+
+        private static List<Grupe> GetAllFromDatabase()
+        {
+            List<Grupe> grupe = new List<Grupe>();
+            try
+            {
+                string path = "database/database.db";
+                using SqliteConnection connection = new SqliteConnection($"Data Source={path}");
+                connection.Open();
+
+                string query = "SELECT * FROM Groups";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+                using SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string naziv = reader.GetString(1);
+                    DateTime datumOsnivanja = reader.GetDateTime(2);
+                    Grupe grupa = new Grupe(id, naziv, datumOsnivanja);
+                    grupe.Add(grupa);
+                }
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+
+            return grupe; // Ensure a value is returned in all code paths
+        }
     }
 
 }
