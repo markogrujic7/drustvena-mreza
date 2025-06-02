14b8578d2db2be52fe699e22e6618a2804e459bd
Marko Grujic <marko.grujic@rocketmail.com>
2025-06-02 22:05:48 +0200

    Napravljena klasa UserDbRepository i prebacen GetAll i GetById

--- drustvene mreze/Controllers/UsersController.cs
+++ drustvene mreze/Controllers/UsersController.cs
@@ -11,72 +11,25 @@ namespace drustvene_mreze.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserRepozitorijum _repo = new();
+        UserDbRepository usersDbRepository = new UserDbRepository();
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var users = GetAllFromDatabase();
+            var users = usersDbRepository.GetAll();
             return Ok(users);
-        }
-
-        private List<User> GetAllFromDatabase()
-        {
-            var users = new List<User>();
-            try
-            {
-                using SqliteConnection connection = new SqliteConnection("Data Source=database/database.db");
-                connection.Open();
-
-                string query = "SELECT Id, Username, Name, Surname, Birthday FROM Users";
-                using SqliteCommand command = new SqliteCommand(query, connection); // Objekat koji omogućava izvršavanje upita
-                using SqliteDataReader reader = command.ExecuteReader(); // Izvršavamo upit i dobijamo rezultat
-
-                while (reader.Read()) // Čitanje rezultata
-                {
-                    int id = Convert.ToInt32(reader["Id"]);
-                    string username = reader["Username"].ToString();
-                    string name = reader["Name"].ToString();
-                    string surname = reader["Surname"].ToString();
-                    DateTime date = Convert.ToDateTime(reader["Birthday"]);
-
-                    var user = new User
-                    {
-                        Id = id,
-                        KorisnickoIme = username,
-                        Ime = name,
-                        Prezime = surname,
-                        DatumRodjenja = date
-                    };
-
-                    users.Add(user);
-                }
-
-            }
-            catch (SqliteException ex)
-            {
-                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Neočekivana greška: {ex.Message}");
-            }
-
-            return users;
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            var user = _repo.GetById(id);
-            return user == null ? NotFound() : Ok(user);
+            var user = usersDbRepository.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
         }
 
         [HttpPost]
---
+++ drustvene mreze/Repository/UserDbRepository.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+using drustvene_mreze.Domen;
+using Microsoft.Data.Sqlite;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace drustvene_mreze.Repository
+{
+    public class UserDbRepository
+    {
+        public List<User> GetAll()
+        {
+            var users = new List<User>();
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection("Data Source=database/database.db");
+                connection.Open();
+
+                string query = "SELECT Id, Username, Name, Surname, Birthday FROM Users";
+                using SqliteCommand command = new SqliteCommand(query, connection); // Objekat koji omogućava izvršavanje upita
+                using SqliteDataReader reader = command.ExecuteReader(); // Izvršavamo upit i dobijamo rezultat
+
+                while (reader.Read()) // Čitanje rezultata
+                {
+                    int id = Convert.ToInt32(reader["Id"]);
+                    string username = reader["Username"].ToString();
+                    string name = reader["Name"].ToString();
+                    string surname = reader["Surname"].ToString();
+                    DateTime date = Convert.ToDateTime(reader["Birthday"]);
+
+                    var user = new User
+                    {
+                        Id = id,
+                        KorisnickoIme = username,
+                        Ime = name,
+                        Prezime = surname,
+                        DatumRodjenja = date
+                    };
+
+                    users.Add(user);
+                 }
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+            return users;
+        }
+
+        public User? GetById(int id)
+        {
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection("Data Source=database/database.db");
+                connection.Open();
+
+                string query = "SELECT Id, Username, Name, Surname, Birthday FROM Users WHERE Id = @id";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                using SqliteDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return new User
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        KorisnickoIme = reader["Username"].ToString(),
+                        Ime = reader["Name"].ToString(),
+                        Prezime = reader["Surname"].ToString(),
+                        DatumRodjenja = Convert.ToDateTime(reader["Birthday"])
+                    };
+                }
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju SQL upita: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+
+            return null;
+        }
+
+    }
+}
